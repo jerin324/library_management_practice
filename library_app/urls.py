06c893ec0d451a6c7f56f424f219ba1defed7b34
muedurr786-virus <muedurr786@gmail.com>
2026-06-17 04:19:24 -0700

    Merge branch 'main' of https://github.com/jerin324/library_management_practice change html

--- library_app/urls.py
+++ library_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('home/', views.home_view, name='home'),
 
     # Authentication
     path('register/', views.register_view, name='register'),
