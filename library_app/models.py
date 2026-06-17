e25ca63c17a8ba73bda72c9a0fd183489b902b45
jerin324 <jerin200059@diit.edu.bd>
2026-06-17 12:01:03 +0600

    first commit

---
+++ library_app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('librarian', 'Librarian'),
+        ('student', 'Student'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, unique=True)
+    total_copies = models.PositiveIntegerField(default=1)
+
+    # Book added by librarian
+    added_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='books_added'
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='borrow_records'
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='borrow_records'
+    )
+    borrow_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
